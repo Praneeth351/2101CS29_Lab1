b309a61a8908407c2dd46bd27b0b0068824127d4
Praneeth351 <110082291+Praneeth351@users.noreply.github.com>
2022-07-28 00:21:32 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -7,7 +7,11 @@ int main(){
   scanf("%c", &operation);
   printf("Enter two numbers: ");
   scanf("%lf %lf", &x, &y);
-  
+  switch (operation) {
+        case '+':
+          printf("%.1lf + %.1lf = %.1lf", x, y, x + y);
+          break;
+    }
 
   return 0;
 }
