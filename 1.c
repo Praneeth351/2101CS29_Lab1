267f967c68351bb28ef0a61eb13a56c135a1e794
Praneeth351 <110082291+Praneeth351@users.noreply.github.com>
2022-07-28 00:21:05 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
+  char operation;
+  double x, y;
+  printf("Enter an operator (+, -, *, /) : ");
+  scanf("%c", &operation);
+  printf("Enter two numbers: ");
+  scanf("%lf %lf", &x, &y);
+  
 
   return 0;
 }
